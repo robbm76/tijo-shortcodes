b8ec1b7a1a26b8c354b37cb5a2542d6bf983d3d1
tijoio <josh@tijo.io>
2016-03-24 12:06:31 -0500

    removed tabs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-//jquery-ui-tabs
 jQuery(document).ready(function($) {
 //jquery-ui-accordion
 $(".gb-accordion").accordion( {
 	collapsible: true,
+	active: false,
 	header: ".gb-accordion-trigger"
 });
 //jquery-ui-tooltip
