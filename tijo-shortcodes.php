f436ffd7152dfdd668e898ac34df04a126e9e900
tijoio <josh@tijo.io>
2016-03-23 21:36:48 -0500

    added cpt and tax helper core classes

--- tijo-shortcodes.php
+++ tijo-shortcodes.php
@@ -25,4 +25,42 @@ function my_jquery_ui_scripts() {
 
 add_action('wp_enqueue_scripts', 'my_jquery_ui_scripts');
 
+/***********************************************************************************
+* cpt/taxonomy
+***********************************************************************************/
 
+// Load CPT Helper
+require_once( plugin_dir_path( __FILE__ ) . 'core-classes/CPT_Core/CPT_Core.php' );
+
+// Load Taxonomy Helper
+require_once( plugin_dir_path( __FILE__ ) . 'core-classes/Taxonomy_Core/Taxonomy_Core.php' );
+
+/* Example
+
+// CPT
+register_via_cpt_core(
+  array(
+    'Single',
+    'Plural',
+    'slides'
+  ),
+  array(
+    'menu_icon' => 'dashicons-slides',
+    'publicly_queryable' => false,
+    'supports' => array( 'title', 'editor', 'thumbnail', 'revisions' )
+  )
+);
+
+// Tax
+register_via_taxonomy_core(
+  array(
+    'Single',
+    'Plural',
+    'slug'
+  ),
+  array(),
+  array(
+    'CPT Name'
+  )
+);
+*/
